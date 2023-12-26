7f9d13f024d1729fed087d621664f8a07f25a215
Pablo <pabrodzamdev@gmail.com>
2023-12-26 17:53:37 +0100

    First commit

---
+++ AvaloniaPeliculas/MainWindow.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+
+namespace AvaloniaPeliculas;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        // Load an image from a file
+        var imageFilePath = "C:\\Users\\pablo\\RiderProjects\\AvaloniaPeliculas\\AvaloniaPeliculas\\assets\\cover_power_of_dog.jpg";
+        var image = new Bitmap(imageFilePath);
+
+        // Do something with the bitmap, e.g., set it as the source of an Image control
+        var imageControl = this.FindControl<Image>("Aa");
+        imageControl.Source = image;
+    }
+}
