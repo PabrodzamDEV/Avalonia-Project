a7cf6b6a744850eb77584aa5f6e3eb8ac6ca1886
Pablo <pabrodzamdev@gmail.com>
2023-12-27 15:55:59 +0100

    Perfecting UI, including sidebar

--- AvaloniaPeliculas/MainWindow.axaml.cs
+++ AvaloniaPeliculas/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Media.Imaging;
 
 namespace AvaloniaPeliculas;
@@ -16,5 +17,11 @@ public partial class MainWindow : Window
         // Do something with the bitmap, e.g., set it as the source of an Image control
         var imageControl = this.FindControl<Image>("Aa");
         imageControl.Source = image;
+    }
+
+    private void OnPanelButtonCliked(object? sender, RoutedEventArgs e)
+    {
+        var splitView = this.FindControl<SplitView>("SplitViewPanel");
+        splitView.IsPaneOpen = !splitView.IsPaneOpen;
     }
 }
---
+++ AvaloniaPeliculas/Utils.cs
@@ -0,0 +1,6 @@
+﻿namespace AvaloniaPeliculas;
+
+public class Utils
+{
+    
+}
